89546d42c1754cffc0e838e0e1d5a58c3b6aae8a
Mike Shelby <mikegshelby@gmail.com>
2020-10-11 21:31:04 -0500

    added password generator code to javascript file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,20 +1,68 @@
-// Assignment code here
+// Alphanumeric Arrays
+const lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+const upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+const NumberArray = ['0','1','2','3','4','5','6','7','8','9']
+const SymbolArray = ['!','@','#','$','%','^','&','*','(',')','_','+','>','<',':','{','}','[',']']
+
+// Function will prompt user to select password criteria
+function criteriaSelect() {
+    var passwordLength = window.prompt("Please enter a password length between 8 and 128 characters");
+      if (passwordLength < 8 || passwordLength >128) {
+        // Alert user if out-of-range number is entered
+        window.alert("Password must be between 8 and 128 characters");
+        criteriaSelect();
+      }
+
+    var includeUppercase = window.confirm("Include uppercase letters in your password");
+
+    var includeNumbers = window.confirm("Include numbers in your password");
+
+    var includeSymbols = window.confirm("Include symbols in your password");
+
+    generatePassword(passwordLength, includeUppercase, includeNumbers, includeSymbols);
+}
+
+// Function will generate a password based on selected criteria
+function generatePassword (passwordLength, includeUppercase, includeNumbers, includeSymbols) {
+
+    var chars = lowerCaseArray
+    if (includeUppercase) {
+      var chars = chars.concat(upperCaseArray);
+    }
+    if (includeNumbers) {
+      var chars = chars.concat(NumberArray);
+    }
+
+    if (includeSymbols) {
+      var chars = chars.concat(SymbolArray);
+    }
+
+    var password = "";
+
+    for (let i = 0; i < passwordLength; i++) {
+      var randomNumber = Math.floor(Math.random() * chars.length);
+
+      password += chars[randomNumber]
+    }
+  passwordText.textContent = password;
+}
 
 
-
+// Get references to the #password element
+var passwordText = document.querySelector("#password");
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", criteriaSelect);
+
+
+
+
+
+
+
+
+
