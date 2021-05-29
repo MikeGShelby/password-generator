03c066bbdd8b8344342e429c735ecd8826322731
Mike Shelby <mikegshelby@gmail.com>
2021-05-29 14:00:54 -0500

    added new styles and javascript functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,65 +1,84 @@
 // Alphanumeric arrays for criteria selection
 const lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 const upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-const NumberArray = ['0','1','2','3','4','5','6','7','8','9']
-const SymbolArray = ['!','@','#','$','%','^','&','*','(',')','_','+','>','<',':','{','}','[',']']
+const numberArray = ['0','1','2','3','4','5','6','7','8','9']
+const symbolArray = ['!','@','#','$','%','^','&','*','(',')','_','+','>','<',':','{','}','[',']']
 
-// Function will prompt user to select password criteria
-function criteriaSelect() {
-    var passwordLength = window.prompt("Please enter a password length between 8 and 128 characters");
-      if (passwordLength < 8 || passwordLength >128) {
-        // Alert user if out-of-range number is entered
-        window.alert("Password must be between 8 and 128 characters");
-        criteriaSelect();
-      }
+function generatePassword (event) {
+  event.preventDefault()
 
-    var includeUppercase = window.confirm("Include uppercase letters in your password");
+  // var chars = lowerCaseArray;
+  var chars = [];
 
-    var includeNumbers = window.confirm("Include numbers in your password");
+  var password = "";
 
-    var includeSymbols = window.confirm("Include symbols in your password");
+  const passwordLength = document.getElementById("password-length").innerHTML;
+  console.log(passwordLength);
 
-    generatePassword(passwordLength, includeUppercase, includeNumbers, includeSymbols);
-}
+  // Add lowercase array if lowercase toggle is selected
+  const includeLowerCase = document.getElementById("include-lowercase");
+    if(includeLowerCase.checked==true) {
+      console.log("include lower case");
+      var chars = chars.concat(lowerCaseArray);
+    } else {
+      console.log("do NOT include lower case");
+    }
 
-// Function will generate a password based on selected criteria
-function generatePassword (passwordLength, includeUppercase, includeNumbers, includeSymbols) {
-    var chars = lowerCaseArray
-
-    // if uppercase characters are selected, then include in password
-    if (includeUppercase) {
+  // Add uppercase array if uppercase toggle is selected
+  const includeUpperCase = document.getElementById("include-uppercase");
+    if(includeUpperCase.checked==true) {
+      console.log("include upper case");
       var chars = chars.concat(upperCaseArray);
-    }
-    // if numbers are selected, then include in password
-    if (includeNumbers) {
-      var chars = chars.concat(NumberArray);
-    }
-    // if symbols are selected, then include in password
-    if (includeSymbols) {
-      var chars = chars.concat(SymbolArray);
+    } else {
+      console.log("do NOT include upper case");
     }
 
-    var password = "";
-
-    // loop through random array items to generate password
-    for (let i = 0; i < passwordLength; i++) {
-      var randomNumber = Math.floor(Math.random() * chars.length);
-
-      password += chars[randomNumber]
+  // Add number array if number toggle is selected
+  const includeNumbers = document.getElementById("include-numbers");
+    if(includeNumbers.checked==true) {
+      console.log("include numbers");
+      var chars = chars.concat(numberArray);
+    } else {
+      console.log("do NOT include numbers");
     }
-  passwordText.textContent = password;
+
+  // Add number array if number toggle is selected
+  const includeSymbols = document.getElementById("include-symbols");
+    if(includeSymbols.checked==true) {
+      console.log("include symbols");
+      var chars = chars.concat(symbolArray);
+    } else {
+      console.log("do NOT include symbols");
+    }
+
+  // loop through random array items to generate password
+  for (let i = 0; i < passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+
+    password += chars[randomNumber]
+  }
+
+  // if no inclusion criteria is selected, then alert the user and do not display any text in password text section
+  if(includeLowerCase.checked==false && includeUpperCase.checked==false && includeNumbers.checked==false && includeSymbols.checked==false ) {
+    console.log("you must select at least one password criteria to include");
+    window.alert("You must select at least one criteria to include in your password");
+    passwordText.textContent = "";
+   } else {
+    passwordText.textContent = password;
+   }
 }
-
 
 // Get references to the #password element
 var passwordText = document.querySelector("#password");
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", criteriaSelect);
+// Add event listener to generate button. Run tests
+generateBtn.addEventListener("click", generatePassword);
+
+
+
 
 
 
